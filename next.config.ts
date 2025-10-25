99997e5d91c41bef1ac4734cc89cba219dfa9f7a
zeroichi <zeroichi@users.noreply.github.com>
2025-10-25 17:23:56 +0900

    GitHub Pages向けにビルド設定変更

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
