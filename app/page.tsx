114e8cd82f71fb94e0bdb3b20f172a239433b216
zeroichi <zeroichi@users.noreply.github.com>
2025-11-22 16:19:39 +0900

    次ランク・目標ランクまでの経験値表示機能追加

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ import { getEffectivity } from "@/utils/utils";
 import Link from "next/link";
 import { useLocalPersistence } from "@/hooks/persist";
 import { PersistData } from "@/types/persist";
-import CSR from "./components/CSR";
 
 const PERSIST_KEY = 'persist-data'
+const INITIAL_PERSIST_DATA: PersistData = { giftCountMap: {}, currentBondLevel: 1, goalBondLevel: 100 }
 
 export default function Home() {
   const { data: masterData, isLoading: masterDataIsLoading } = useMasterData()
@@ -25,7 +25,10 @@ export default function Home() {
   const [selectedStudent, setSelectedStudent] = useState(persistData?.selectedStudentId)
   /** 絆ランク入力のエラー */
   const [errorMessage, setErrorMessage] = useState("")
+  /** 現在の絆ランク TODO:生徒ごとに変えられるようにする */
   const [currentBondLevel, setCurrentBondLevel] = useState(persistData?.currentBondLevel ?? 1)
+  /** 目標の絆ランク TODO:生徒ごとに変えられるようにする */
+  const [goalBondLevel, setGoalBondLevel] = useState(persistData?.goalBondLevel ?? 100)
 
   /** 経験値テーブルマップ (Level => Exp) */
   const bondExpMap = useMemo(() => {
@@ -105,7 +108,6 @@ export default function Home() {
 
   /** 現在の絆ランクが変更された時のイベントハンドラ */
   const onChangeCurrentBondLevel = (currentLevelStr: string) => {
-    // console.log("onChangeCurrentBondLevel", currentLevelStr)
     const currentLevel = parseInt(currentLevelStr)
     if (isNaN(currentLevel) || currentLevel < 1 || currentLevel > 100) {
       setErrorMessage("絆ランクは1以上100以下の整数を入力してください")
@@ -116,21 +118,53 @@ export default function Home() {
     }
   }
 
-  /** 絆ランクの期待値を再計算する */
-  const calcExpectedBondLevel = useMemo(() => {
-    // console.log("現在の絆ランク:", currentBondLevel)
+  /** 目標の絆ランクが変更された時のイベントハンドラ */
+  const onChangeGoalBondLevel = (goalLevelStr: string) => {
+    const goalLevel = parseInt(goalLevelStr)
+    if (isNaN(goalLevel) || goalLevel < 1 || goalLevel > 100) {
+      setErrorMessage("絆ランクは1以上100以下の整数を入力してください")
+    } else {
+      setErrorMessage("")
+      setGoalBondLevel(goalLevel)
+      setPersistData({ ...persistData ?? INITIAL_PERSIST_DATA, goalBondLevel: goalLevel })
+    }
+  }
+
+  /** 現在の絆ランクから贈り物経験値を合計した絆経験値 */
+  const expectedExp = useMemo(() => {
     if (isNaN(currentBondLevel)) return undefined
-
     const currentExp = bondExpMap.get(currentBondLevel)
-    // console.log("現在の絆経験値:", currentExp)
     if (currentExp == undefined) return undefined
+    return currentExp + totalExp
+  }, [bondExpMap, currentBondLevel, totalExp])
 
-    const expectedExp = currentExp + totalExp
-    // console.log("期待の絆経験値:", expectedExp)
-    if (bondExpTable?.bondExpTable == undefined) return undefined
+  /** 絆ランクの期待値 */
+  const calcExpectedBondLevel = useMemo(() => {
+    if (expectedExp == undefined || bondExpTable?.bondExpTable == undefined) return undefined
 
-    return bondExpTable.bondExpTable.findLastIndex((v) => v <= expectedExp) + 1
-  }, [bondExpMap, bondExpTable, currentBondLevel, totalExp])
+    return bondExpTable?.bondExpTable.findLastIndex((v) => v <= expectedExp) + 1
+  }, [bondExpTable?.bondExpTable, expectedExp])
+
+  /** 次のランクまで必要な経験値 */
+  const requiredExpToNextLevel = useMemo<[number, number] | undefined>(() => {
+    if (calcExpectedBondLevel == undefined
+      || expectedExp == undefined
+      || calcExpectedBondLevel < 1
+      || calcExpectedBondLevel >= 100
+      || calcExpectedBondLevel >= goalBondLevel) return undefined
+    const nextLevelExp = bondExpTable?.bondExpTable[calcExpectedBondLevel]
+    if (nextLevelExp == undefined) return undefined
+    return [calcExpectedBondLevel + 1, nextLevelExp - expectedExp]
+  }, [calcExpectedBondLevel, expectedExp, goalBondLevel, bondExpTable?.bondExpTable])
+
+  /** 目標のランクまで必要な経験値 */
+  const requiredExpToGoalLevel = useMemo(() => {
+    if (expectedExp == undefined) return undefined
+    const goalLevelExp = bondExpTable?.bondExpTable[goalBondLevel - 1]
+    if (goalLevelExp == undefined) return undefined
+    const requiredExp = goalLevelExp - expectedExp
+    return requiredExp <= 0 ? undefined : requiredExp
+  }, [expectedExp, goalBondLevel, bondExpTable?.bondExpTable])
 
   return (
     <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -143,6 +177,9 @@ export default function Home() {
         <div id="student-selector" className="my-2">
           生徒を選択: <StudentSelector students={masterData?.students ?? []} onChange={onSelectedStudentChange} initialValue={persistData?.selectedStudentId} />
         </div>
+        <p className="text-xs">
+          ゲーム内の「アイテム」→フィルター「贈り物」→ソート「デフォルト」で所持している贈り物数を調べられます。PCではTabキーで入力欄を移動できます。
+        </p>
         {/* 贈り物の数を入力するフォーム */}
         {/* grid-cols-5 gap-2 */}
         <div id="gift-count-form" className="grid grid-cols-3 md:grid-cols-6 lg:grid-cols-10 gap-2">
@@ -158,8 +195,8 @@ export default function Home() {
           総絆経験値 {bondExpMap.get(currentBondLevel) != undefined ? ((bondExpMap.get(currentBondLevel) ?? 0) + totalExp) : "-"}
         </div>
         {/* 現在の絆ランクと、アイテムで到達できる絆ランクを表示 */}
-        <div id="bond-level-indicator" className="flex items-baseline gap-8 w-full justify-center">
-          <TextField variant="standard" label="現在の絆ランク" className="w-32" type="number"
+        <div id="bond-level-indicator" className="flex items-baseline gap-2 w-full justify-center">
+          <TextField variant="standard" label="現在の絆ランク" className="w-30" type="number"
             defaultValue={currentBondLevel}
             onChange={(e) => onChangeCurrentBondLevel(e.target.value)}
             slotProps={{
@@ -173,8 +210,22 @@ export default function Home() {
               }
             }} />
           <KeyboardDoubleArrowRightIcon className="block" />
-          <TextField variant="standard" label="到達できる絆ランク" disabled className="w-32"
+          <TextField variant="standard" label="到達できる絆ランク" disabled className="w-30"
             value={(errorMessage || !calcExpectedBondLevel) && "-" || calcExpectedBondLevel}
+            slotProps={{
+              /* テキストボックスの頭にハートマーク */
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <FavoriteIcon className="text-red-300" />
+                  </InputAdornment>
+                )
+              }
+            }} />
+          <KeyboardDoubleArrowRightIcon className="block" />
+          <TextField variant="standard" label="目標の絆ランク" className="w-30" type="number"
+            defaultValue={goalBondLevel}
+            onChange={(e) => onChangeGoalBondLevel(e.target.value)}
             slotProps={{
               /* テキストボックスの頭にハートマーク */
               input: {
@@ -189,7 +240,16 @@ export default function Home() {
         {errorMessage && <div className="my-4 p-4 border-2 border-red-200 rounded-lg bg-red-50 text-red-600">
           {errorMessage}
         </div>}
-        <div className="mt-8 pt-4 border-t-2 border-red-200 w-full">
+        {requiredExpToNextLevel &&
+          <div className="mt-4 text-sm">
+            次のランク<FavoriteIcon className="text-red-300" fontSize="inherit" />{requiredExpToNextLevel[0]} までに必要な経験値: {requiredExpToNextLevel[1]}
+          </div>
+        }
+        {requiredExpToGoalLevel &&
+          <div className="mt-2 text-sm">
+            目標のランク<FavoriteIcon className="text-red-300" fontSize="inherit" />{goalBondLevel} までに必要な経験値: {requiredExpToGoalLevel}
+          </div>
+        }        <div className="mt-8 pt-4 border-t-2 border-red-200 w-full">
           ※ブラウザにのみデータを保存しています。ブラウザのキャッシュ等をクリアすると値は初期化されます。<br />
           Contact: @zeroichi
           &nbsp;
@@ -200,11 +260,18 @@ export default function Home() {
         <div className="mt-8 pt-4 border-t-4 border-gray-200 w-full">
           <h2>更新履歴</h2>
           <ul className="list-disc ml-6">
+            <li>2025/11/22
+              <ul>
+                <li>次の絆ランク経験値の表示機能を追加</li>
+                <li>目標の絆ランク入力と、目標ランクまでの経験値の表示機能を追加</li>
+              </ul>
+            </li>
             <li>2025/11/20
               <ul>
                 <li>新規実装生徒「タカネ」「ヤクモ」追加</li>
                 <li>&quot;絆ランク&quot;に用語を統一</li>
                 <li>高級贈り物と通常贈り物の違いが分かるように背景色を変更</li>
+                <li>スマホ向けにレイアウト微調整(レスポンシブ対応)</li>
               </ul>
             </li>
             <li>2025/11/16
