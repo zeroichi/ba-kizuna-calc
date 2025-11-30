68f7ad39b639a6b3e0cf173d157b4d52d4a08e5e
zeroichi <zeroichi@users.noreply.github.com>
2025-11-30 18:19:57 +0900

    next-seo導入

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useLocalPersistence } from "@/hooks/persist";
 import { useBondExpTable, useMasterData } from "@/hooks/useMasterData";
 import { PersistData } from "@/types/persist";
 import MainPage from "./components/pages/MainPage";
+import { SoftwareApplicationJsonLd } from "next-seo";
 
 const PERSIST_KEY = 'persist-data'
 const INITIAL_PERSIST_DATA: PersistData = { giftCountMap: {}, currentBondLevel: 1, goalBondLevel: 100, tailorStoneCount: 0 }
@@ -14,12 +15,48 @@ export default function Home() {
   const [persistData, setPersistData] = useLocalPersistence<PersistData>(PERSIST_KEY, INITIAL_PERSIST_DATA)
 
   return (
-    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      {
-        masterData.data && bondExpTable.data && persistData
-          ? <MainPage masterData={masterData.data} bondExpTable={bondExpTable.data} persistData={persistData} onPersistData={setPersistData} />
-          : <div className="h-screen w-screen flex justify-center items-center">データをロード中...</div>
-      }
-    </div>
+    <>
+      <SoftwareApplicationJsonLd
+        type="WebApplication"
+        name="ブルアカ 絆ランク 計算機/シミュレータ"
+        url="https://ako.zeroichi.jp/"
+        applicationCategory="UtilitiesApplication"
+        offers={[
+          {
+            price: 0,
+            priceCurrency: "JPY",
+            availability: "https://schema.org/InStock",
+          },
+        ]}
+        // aggregateRating={{
+        //   ratingValue: 5.0,
+        //   ratingCount: 1,
+        // }}
+        author={
+          {
+            "@type": "Person",
+            name: "zeroichi",
+            url: "https://www.zeroichi.jp/",
+          }
+        }
+        datePublished="2025-11-20T15:00:00+09:00"
+        dateModified="2025-11-30T18:00:00+09:00"
+        screenshot={{
+          url: "https://public.ako.zeroichi.jp/ba-kizuna-calc.png",
+          caption: "絆ランク 計算画面",
+        }}
+        featureList={[
+          "ブルアカ 絆ランク 計算",
+          "ブルアカ 絆ランク シミュレーション",
+        ]}
+      />
+      <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        {
+          masterData.data && bondExpTable.data && persistData
+            ? <MainPage masterData={masterData.data} bondExpTable={bondExpTable.data} persistData={persistData} onPersistData={setPersistData} />
+            : <div className="h-screen w-screen flex justify-center items-center">データをロード中...</div>
+        }
+      </div>
+    </>
   );
 }
