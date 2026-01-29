b6937c1af65e1db48c4235aaf952d6231efc1177
zeroichi <zeroichi@users.noreply.github.com>
2026-01-29 23:09:30 +0900

    新規生徒ケイ・アリス(臨戦)追加

--- app/components/organisms/ReleaseNotes.tsx
+++ app/components/organisms/ReleaseNotes.tsx
@@ -2,6 +2,11 @@ export const ReleaseNotes = () => {
   return <div className="mt-4 pt-4 border-t-4 border-gray-200 w-full text-sm text-gray-600">
     <h2>更新履歴</h2>
     <ul className="list-disc ml-6">
+      <li>2026/01/29
+        <ul>
+          <li>新規実装生徒「ケイ」「アリス（臨戦）」追加</li>
+        </ul>
+      </li>
       <li>2026/01/22
         <ul>
           <li>新規実装生徒「ヒマリ（臨戦）」「トキ（臨戦）」追加</li>
