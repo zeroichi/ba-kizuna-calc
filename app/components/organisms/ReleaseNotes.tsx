3601a774ef859545038d48959e56c64c64b22ace
zeroichi <zeroichi@users.noreply.github.com>
2026-02-14 21:47:31 +0900

    新規生徒ユズ(臨戦)・エイミ(臨戦)追加

--- app/components/organisms/ReleaseNotes.tsx
+++ app/components/organisms/ReleaseNotes.tsx
@@ -2,6 +2,11 @@ export const ReleaseNotes = () => {
   return <div className="mt-4 pt-4 border-t-4 border-gray-200 w-full text-sm text-gray-600">
     <h2>更新履歴</h2>
     <ul className="list-disc ml-6">
+      <li>2026/02/14
+        <ul>
+          <li>新規実装生徒「ユズ（臨戦）」「エイミ（臨戦）」追加</li>
+        </ul>
+      </li>
       <li>2026/02/12
         <ul>
           <li>達成日シミュレーションの贈り物セット月一PKG・製造用月一PKGを購入数指定できるように修正</li>
